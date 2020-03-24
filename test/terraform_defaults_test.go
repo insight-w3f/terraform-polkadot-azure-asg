64a5755ca29e567f69660f3674c36588dbe749e3
rob <robcannonxyz@gmail.com>
2020-03-23 20:34:17 -0700

    adding mod for tests and cleaning triggers

--- test/terraform_defaults_test.go
+++ test/terraform_defaults_test.go
@@ -1,20 +1,23 @@
 package test
 
 import (
-	"github.com/gruntwork-io/terratest/modules/aws"
+	"fmt"
+	"github.com/gruntwork-io/terratest/modules/retry"
+	"github.com/gruntwork-io/terratest/modules/shell"
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	test_structure "github.com/gruntwork-io/terratest/modules/test-structure"
 	"log"
 	"os"
 	"path"
+	"strings"
 	"testing"
+	"time"
 )
 
 func TestTerraformDefaults(t *testing.T) {
 	t.Parallel()
 
 	exampleFolder := test_structure.CopyTerraformFolderToTemp(t, "../", "examples/defaults")
-	awsRegion := aws.GetRandomStableRegion(t, nil, nil)
 
 	cwd, err := os.Getwd()
 	if err != nil {
@@ -29,9 +32,8 @@ func TestTerraformDefaults(t *testing.T) {
 	terraformOptions := &terraform.Options{
 		TerraformDir: exampleFolder,
 		Vars: map[string]interface{}{
-			"aws_region":         awsRegion,
 			"public_key_path":    publicKeyPath,
-			"private_key_path":   privateKeyPath,
+			"chain": "dev",
 		},
 	}
 
@@ -41,5 +43,47 @@ func TestTerraformDefaults(t *testing.T) {
 
 	test_structure.RunTestStage(t, "setup", func() {
 		terraform.InitAndApply(t, terraformOptions)
+	})
+
+	test_structure.RunTestStage(t, "validate", func() {
+		terraformOptions := test_structure.LoadTerraformOptions(t, exampleFolder)
+		testLbEndpoints(t, terraformOptions)
+	})
+}
+
+func testLbEndpoints(t *testing.T, terraformOptions *terraform.Options) {
+
+	loadBalancerIp := terraform.Output(t, terraformOptions, "public_ip")
+
+	cmd := fmt.Sprintf("curl -sL --data-binary '{\"id\":1,\"jsonrpc\":\"2.0\",\"method\":\"system_health\", \"params\":[]}' -H 'content-type:application/json' -w %%{http_code} -o /dev/null http://%s:9933", loadBalancerIp)
+
+	expectedStatus := "200"
+
+	command := shell.Command{
+		Command:           cmd,
+		Args:              nil,
+		WorkingDir:        ".",
+		Env:               nil,
+		OutputMaxLineSize: 0,
+	}
+
+	description := fmt.Sprintf("curl to LB %s with error command", loadBalancerIp)
+	maxRetries := 30
+	timeBetweenRetries := 1 * time.Second
+
+	// Verify that we can SSH to the Instance and run commands
+	retry.DoWithRetry(t, description, maxRetries, timeBetweenRetries, func() (string, error) {
+
+		outputStatus, err := shell.RunCommandAndGetOutputE(t, command)
+
+		if err != nil {
+			return "", err
+		}
+
+		if strings.TrimSpace(outputStatus) != expectedStatus {
+			return "", fmt.Errorf("expected SSH command to return '%s' but got '%s'", expectedStatus, outputStatus)
+		}
+
+		return "", nil
 	})
 }
