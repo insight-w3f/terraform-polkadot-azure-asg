768104f1128d71cee255cda50b683eb41c0fa8bb
rob <robcannonxyz@gmail.com>
2020-07-20 15:31:16 -0700

    rm label module

--- variables.tf
+++ variables.tf
@@ -44,35 +44,18 @@ variable "k8s_scale_set" {
   default     = ""
 }
 
-########
+
+#######
 # Label
-########
-variable "environment" {
-  description = "The environment"
-  type        = string
-  default     = ""
-}
-
-variable "namespace" {
-  description = "The namespace to deploy into"
-  type        = string
-  default     = ""
-}
-
-variable "stage" {
-  description = "The stage of the deployment"
-  type        = string
-  default     = ""
+#######
+variable "tags" {
+  description = "Tags in the form of key value pairs to associate with resources"
+  type        = map(string)
+  default     = {}
 }
 
 variable "network_name" {
   description = "The network name, ie kusama / mainnet"
-  type        = string
-  default     = ""
-}
-
-variable "owner" {
-  description = "Owner of the infrastructure"
   type        = string
   default     = ""
 }
